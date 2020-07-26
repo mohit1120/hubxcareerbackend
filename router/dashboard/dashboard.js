885583637783ea7415fcd433eaa89c905af721af
ghostman <sushilkumarc627@gmail.com>
2020-07-26 15:10:57 -0400

    make it better

--- router/dashboard/dashboard.js
+++ router/dashboard/dashboard.js
@@ -1,112 +1,101 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const localStorage = require('localStorage')
+const jwt = require("jsonwebtoken");
+const localStorage = require("localStorage");
 
 //To get secret key
-require('dotenv').config()
+require("dotenv").config();
 const jwt_key = process.env.secret_key;
 
 //For event schema
-const Event = require('../../model/event/event');
+const Event = require("../../model/event/event");
 
-router.post('/addevent', (req, res)=>{
-
-
-    Event.find({id: req.body.id})
+router.post("/addevent", (req, res) => {
+  Event.find({ id: req.body.id })
     .exec()
-    .then(event =>{
-try {
-    req.body = JSON.parse(req.body); 
-} catch (e) {
-   console.log(req.body);
-}
+    .then((event) => {
+      try {
+        req.body = JSON.parse(req.body);
+      } catch (e) {
+        console.log(req.body);
+      }
+      console.log("Event alreday exist:", event);
 
-        if(event && event.length > 0) {
-            console.log("Event alreday exist:", user);
-            return res.status(200).json({
-                message: "Event already exist",
-                success: false
-            })
-
-        } else{
-
-    const event = new Event({
-        id: req.body.id,
-        name: req.body.name,
-        date: new Date(),
-        description: req.body.description,
-        created_by: req.body.email
-    })
-
-
-
-    
-    event.save()
-    .then(event=>{
-        console.log("Inserted event is:", event);
+      if (event && event.length > 0) {
+        console.log("Event alreday exist:", event);
         return res.status(200).json({
-            message: "",
-            success: true
-        })
+          message: "Event already exist",
+          success: false,
+        });
+      } else {
+        const event = new Event({
+          id: req.body.id,
+          name: req.body.name,
+          date: new Date(),
+          description: req.body.description,
+          created_by: req.body.email,
+        });
+
+        event
+          .save()
+          .then((event) => {
+            console.log("Inserted event is:", event);
+            return res.status(200).json({
+              message: "",
+              success: true,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json({
+              message: "Something went wrong",
+              success: false,
+            });
+          });
+      }
     })
-    .catch(err=>{
-        console.log(err);
-        return res.status(500).json({
-            message: "Something went wrong",
-            success: false
-        })
-    });
-
-
-}
-})
-.catch(err =>{
-    console.log("Error occured");
-    res.status(500).json({
+    .catch((err) => {
+      console.log("Error occured");
+      res.status(500).json({
         error: err,
-        message: "Something went wrong"
-    })
-})
-
-})
-
+        message: "Something went wrong",
+      });
+    });
+});
 
 //To get all events data
-router.get('/eventlist', (req, res)=>{
-    Event.find({})
+router.get("/eventlist", (req, res) => {
+  Event.find({})
     .exec()
-    .then(events =>{
-        console.log("Details of an event is:", events);
-        return res.status(200).json({
-             events:events,
-             success:true
-        })
+    .then((events) => {
+      console.log("Details of an event is:", events);
+      return res.status(200).json({
+        events: events,
+        success: true,
+      });
     })
-    .catch(err =>{
-        message: "No event occured"
-    })
-})
+    .catch((err) => {
+      message: "No event occured";
+    });
+});
 
 //To get event data created by user
-router.get('/event', (req, res)=>{
-    console.log(req.params.email);
-    Event.find({created_by: req.query.email})
+router.get("/event", (req, res) => {
+  console.log(req.params.email);
+  Event.find({ created_by: req.query.email })
     .exec()
-    .then(events =>{
-        console.log("Details of an event is:", events);
-        return res.status(200).json({
-             events:events,
-             success:true
-        })
+    .then((events) => {
+      console.log("Details of an event is:", events);
+      return res.status(200).json({
+        events: events,
+        success: true,
+      });
     })
-    .catch(err =>{
-        message: "No event occured"
-    })
-})
-
-
+    .catch((err) => {
+      message: "No event occured";
+    });
+});
 
 module.exports = router;
